dbb56a941197405c175b1204f5192a13916a5cdb
ViTo Tse <vitotse88@km-it.cn>
2021-08-27 23:38:05 +0800

    add format playground

---
+++ main.playground/Pages/Formatter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+
+let numberFormatter = NumberFormatter.init()
+
+let number = 123
+
+let numberString = numberFormatter.string(for: number)
