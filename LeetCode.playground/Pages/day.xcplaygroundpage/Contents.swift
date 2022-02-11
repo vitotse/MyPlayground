0968fedd562e5d8e1479a002338a557903beb3f7
ViTo Tse <vitotse88@gmail.com>
2022-02-11 23:20:20 +0800

    add leetcode answer 1984;

--- LeetCode.playground/Pages/day.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/day.xcplaygroundpage/Contents.swift
@@ -24,4 +24,24 @@ func countKDifference(_ nums: [Int], _ k: Int) -> Int {
 
 countKDifference([1,2,2,1], 1)
 
+/// 1984. 学生分数的最小差值
+/// https://leetcode-cn.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+func minimumDifference(_ nums: [Int], _ k: Int) -> Int {
+    
+    var answer = Int.max
+    let sort = nums.sorted { a, b in
+        return a > b
+    }
+    
+    var i = 0
+    for _ in (i+k-1)..<nums.count {
+        answer = min(answer, abs(sort[i + k - 1] - sort[i]))
+        i += 1
+    }
+    
+    return answer
+}
+
+minimumDifference([90,3,2,4], 2)
+
 //: [Next](@next)
