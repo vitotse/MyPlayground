d796d751aaac8289172d0fbc1ea4dfabe19011a0
ViTo Tse <vitotse88@gmail.com>
2022-04-03 00:56:23 +0800

    add answers

--- LeetCode-Offer.playground/Pages/Offer-15.xcplaygroundpage/Contents.swift
+++ LeetCode-Offer.playground/Pages/Offer-15.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@
 
 import Foundation
 
+/// 剑指 Offer 15. 二进制中1的个数
+/// https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/solution/mian-shi-ti-15-er-jin-zhi-zhong-1de-ge-shu-wei-yun/
 class Solution {
     func hammingWeight(_ n: Int) -> Int {
         var number = n
@@ -20,5 +22,24 @@ class Solution {
 
 let solution = Solution()
 let count = solution.hammingWeight(11)
+
+
+/// 位运算
+func hammingWeightWithAndOperate(_ n: Int) -> Int {
+    var ret = 0
+    
+    for i in 0..<32 {
+        
+        let res = n & (1 << i)
+        print("\(res)")
+//        if res > 0 {
+//            ret ++
+//        }
+        
+    }
+    return ret
+}
+
+let countO = hammingWeightWithAndOperate(11)
 
 //: [Next](@next)
